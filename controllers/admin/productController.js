f32d4aa31146b4dcb7e5fa988dcd51f74beb1078
GianTee20 <60635471+GianTee20@users.noreply.github.com>
2021-05-02 17:25:30 +0800

    added admin-product

---
+++ controllers/admin/productController.js
@@ -0,0 +1,80 @@
+const database = require("../../models/db");
+const product = require("../../models/schemas/productSchema");
+const path = require('path');
+
+var productController = {
+
+    getProducts : function(req, res) {
+        const projection = 'name description image productId';
+        database.findMany(product, {}, projection, function(result) {
+            var loggedIn = false;
+
+            if(req.session.userId) loggedIn = true;
+            else loggedIn = false;
+
+
+            if(result != null) {
+                const details = {
+                    result,
+                    title: "Admin | Admin Products",
+                    loggedIn: loggedIn,
+                    userId: req.session.userId,
+                    name: req.session.name,
+                    error: null,
+                    path
+                };
+                res.render('admin/admin-product', details);
+            }
+            else {
+                const details = {
+                    result,
+                    title: "Admin | No Products Found",
+                    loggedIn: loggedIn,
+                    userId: req.session.userId,
+                    name: req.session.name,
+                    error: "No Products Found.",
+                    path
+                };
+                console.log(result);
+                res.render('admin/admin-product', details);
+            }
+        });
+    },
+
+    getProduct : function(req, res) {
+        const projection = '';
+        const query = {productId: req.params.productId};
+        database.findOne(product, query, projection, function(result) {
+            var loggedIn = false;
+
+            if(req.session.userId) loggedIn = true;
+            else loggedIn = false;
+
+            if(result != null) {
+
+                const details = {
+                    result,
+                    title: "Baked Goods | " + result.name,
+                    loggedIn: loggedIn,
+                    userId: req.session.userId,
+                    name: req.session.name,
+                    error: null
+                };
+
+                res.render('admin/product', details);
+            } else {
+                const details = {
+                    result,
+                    title: "Baked Goods | Error 404",
+                    loggedIn: loggedIn,
+                    userId: req.session.userId,
+                    name: req.session.name,
+                    error: "404: Page not Found."
+                };
+                res.render('admin/error', details);
+            }
+        });
+    }
+}
+
+module.exports = productController;
--- routes/admin.js
+++ routes/admin.js
@@ -4,16 +4,20 @@ const adminController = require('../controllers/admin/adminController');
 
 const adminLoginController = require('../controllers/admin/adminLoginController');
 
+const adminProductController = require('../controllers/admin/productController');
+
 
 const app = express.Router();
 
 app.get('/admin', function (req, res) {
-    res.render('adminlogin', {title: 'Admin Login'});
+    res.render('/adminlogin', {title: 'Admin Login'});
 });
 
 app.get('/adminlogin', adminLoginController.getLogin);
 
 app.post('/adminlogin', adminLoginController.postLogin);
+
+app.get('/admin-product', adminProductController.getProducts);
 
 app.use(adminController.error);
 
